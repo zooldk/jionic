b9de49b5f61bf205ea2920c0f13ac8814e8242dc
Steffen Larsen <slarsen@braintrust.dk>
2015-04-06 11:57:27 +0300

    Added push status

--- src/main/java/dk/braintrust/ionic/api/push/Ionic.java
+++ src/main/java/dk/braintrust/ionic/api/push/Ionic.java
@@ -6,6 +6,7 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
@@ -18,9 +19,11 @@ import org.apache.http.util.EntityUtils;
  *
  */
 public class Ionic {
-	private static String URL = "https://push.ionic.io/api/v1/push";
-	private static HttpClient client = null;
-	private static HttpPost request = null;
+	private static String PUSH_URL = "https://push.ionic.io/api/v1/push";
+	private static String PUSH_STATUS_URL = "https://push.ionic.io/api/v1/status/";	
+	private static String APP_ID = "";
+	private static byte[] SECRET_ENCODED = null;
+	
 	
 	/**
 	 * These two parameters can be found at https://apps.ionic.io/ in your specific app.
@@ -28,12 +31,8 @@ public class Ionic {
 	 * @param secret (api key - secret)
 	 */
 	public Ionic(String appId, String secret) {
-		byte[] encodedSecret = Base64.encodeBase64(secret.getBytes());				
-		request = new HttpPost(URL);
-		request.addHeader("X-Ionic-Application-Id", appId);
-		request.addHeader("content-type", ContentType.APPLICATION_JSON.toString());				
-		request.addHeader("Authorization", "Basic " + new String(encodedSecret));		
-		client = HttpClientBuilder.create().build();
+		SECRET_ENCODED = Base64.encodeBase64(secret.getBytes());
+		APP_ID = appId;		
 	}
 	
 	/**
@@ -45,9 +44,31 @@ public class Ionic {
 	 * @throws IOException
 	 */
 	public String push(String notification) throws ClientProtocolException, IOException {
+		HttpPost request = new HttpPost(PUSH_URL);
+		request.addHeader("X-Ionic-Application-Id", APP_ID);
+		request.addHeader("content-type", ContentType.APPLICATION_JSON.toString());				
+		request.addHeader("Authorization", "Basic " + new String(SECRET_ENCODED));		
+		HttpClient client = HttpClientBuilder.create().build();
 		request.setEntity(new StringEntity(notification));
 		HttpEntity entity = client.execute(request).getEntity();
-		return  EntityUtils.toString(entity, "UTF-8");
+		return EntityUtils.toString(entity, "UTF-8");
 	}
+
+	/**
+	 * Get a status of a given push message
+	 * @param messageId
+	 * @throws IOException 
+	 * @throws ClientProtocolException 
+	 */
+	public String getPushStatus(String messageId) throws ClientProtocolException, IOException {
+		HttpGet request = new HttpGet(PUSH_STATUS_URL + messageId);
+		request.addHeader("X-Ionic-Application-Id", APP_ID);
+		request.addHeader("content-type", ContentType.APPLICATION_JSON.toString());				
+		request.addHeader("Authorization", "Basic " + new String(SECRET_ENCODED));		
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpEntity entity = client.execute(request).getEntity();
+		return EntityUtils.toString(entity, "UTF-8");
+	}
+	
 
 }
